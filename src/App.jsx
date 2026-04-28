139fbdf4376bb3659624db8f3c6f127a34b102de
JAMES2021-CREATOR <dahunsijames2021@email.com>
2026-04-28 14:53:21 +0100

    fix gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import viteLogo from './assets/vite.svg'
 
 import './App.css'
-import Hero from './component/hero'
+import Hero from './component/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
