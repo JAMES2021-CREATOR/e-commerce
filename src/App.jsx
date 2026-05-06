a989c6902b71d8e5c9fe3c752af7ab99120a0c98
JAMES2021-CREATOR <dahunsijames2021@email.com>
2026-05-06 14:45:52 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { useState } from 'react'
 import viteLogo from './assets/vite.svg'
-
 import './App.css'
 import Home from './_component/Home'
-
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Home/>
+      <Home />
     </>
   )
 }
